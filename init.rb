9230e33de03ff3df9989429856f9cde540f5f959
Gregor Schmidt <ruby@schmidtwisser.de>
2011-06-20 14:29:30 +0200

    Releasing 0.2.1
    * using decimal asset_id instead of hexadecimal
    
    This change should ease conditional setting of expires headers, since the regexp will become easier

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Dispatcher.to_prepare do
   require_dependency 'finn_release/finn_release'
 
   if FinnRelease::FinnRelease.release_time.present?
-    ENV["RAILS_ASSET_ID"] = FinnRelease::FinnRelease.release_time.to_i.to_s(16)
+    ENV["RAILS_ASSET_ID"] = FinnRelease::FinnRelease.release_time.to_i.to_s
   end
 end
 
@@ -14,5 +14,5 @@ Redmine::Plugin.register :chiliproject_finn_release do
   author 'Stephan Eckardt @ finnlabs'
   author_url 'http://www.finn.de'
   description 'This plugin makes the Finn release version information available to Chiliproject'
-  version '0.2.0'
+  version '0.2.1'
 end
