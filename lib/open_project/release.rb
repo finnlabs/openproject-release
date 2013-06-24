04c4b0d131b1c847d29c1addbb409e492e8dd96a
Christian Ratz <c.ratz@finn.de>
2013-06-24 16:56:06 +0200

    Final name and lib structure, display version
    and release date in OpenProject footer
    
    - used translations from old customize plugin
    - changed source path of parsed files to rails_root/config

--- lib/open_project/release.rb
+++ lib/open_project/release.rb
@@ -1,25 +1,25 @@
-require 'openproject_release/engine'
+module OpenProject
+  module Release
+    require 'open_project/release/engine'
 
-module OpenprojectRelease
-  module OpenprojectRelease
     def self.version
-      @version ||= read_file('../../RELEASE_VERSION')
+      @version ||= File.read("#{Rails.root.join('config').to_s}/RELEASE_VERSION")
     end
-    
+
     def self.release_date
       @date ||= begin
-        s = read_file('../../RELEASE_DATE')
+        s = File.read("#{Rails.root.join('config').to_s}/RELEASE_DATE")
         Date.parse(s) if s
       end
     end
 
     def self.release_time
       @time ||= begin
-        s = read_file('../../RELEASE_DATE')
+        s = File.read("#{Rails.root.join('config').to_s}/RELEASE_DATE")
         Time.parse(s) if s
       end
     end
-    
+
     private
     def self.read_file( filename )
       File.read( File.expand_path(File.dirname(__FILE__) + filename ) ) rescue nil
