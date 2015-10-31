d61771d6b32a5e06c717236e9a24b5171a4a8635
Stefan Botzenhart <sb@basiszwo.com>
2015-10-31 09:46:53 +0100

    Fix rubocop issues

--- lib/open_project/release.rb
+++ lib/open_project/release.rb
@@ -3,28 +3,26 @@ module OpenProject
     require 'open_project/release/engine'
 
     def self.version
-      begin
-        @version ||= File.read("#{Rails.root.join('config').to_s}/RELEASE_VERSION")
-      rescue
-        @version = "no release version"
-      end
+      @version ||= File.read("#{Rails.root.join('config')}/RELEASE_VERSION")
+    rescue
+      @version = 'no release version'
     end
 
     def self.release_date
       @date ||= begin
-        s = File.read("#{Rails.root.join('config').to_s}/RELEASE_DATE")
+        s = File.read("#{Rails.root.join('config')}/RELEASE_DATE")
         Date.parse(s) if s
       rescue
-        @date = "no release date"
+        @date = 'no release date'
       end
     end
 
     def self.release_time
       @time ||= begin
-        s = File.read("#{Rails.root.join('config').to_s}/RELEASE_DATE")
+        s = File.read("#{Rails.root.join('config')}/RELEASE_DATE")
         Time.parse(s) if s
       rescue
-        @date = "no release time"
+        @date = 'no release time'
       end
     end
 
--- lib/open_project/release/engine.rb
+++ lib/open_project/release/engine.rb
@@ -4,7 +4,7 @@ module OpenProject::Release
     config.autoload_paths += Dir["#{config.root}/lib/"]
 
     initializer 'release.register_test_paths' do |app|
-      app.config.plugins_to_test_paths << self.root
+      app.config.plugins_to_test_paths << root
     end
 
     config.to_prepare do
@@ -15,21 +15,21 @@ module OpenProject::Release
       spec = Bundler.environment.specs['openproject-release'][0]
 
       if OpenProject::Release.release_time.present?
-        ENV["RAILS_ASSET_ID"] = OpenProject::Release.release_time.to_i.to_s
+        ENV['RAILS_ASSET_ID'] = OpenProject::Release.release_time.to_i.to_s
       end
 
       unless Redmine::Plugin.registered_plugins.include?(:openproject_release)
         Redmine::Plugin.register :openproject_release do
           name 'OpenProject Release Plugin'
-          author ((spec.authors.kind_of? Array) ? spec.authors[0] : spec.authors)
+          author ((spec.authors.is_a? Array) ? spec.authors[0] : spec.authors)
           author_url spec.homepage
           description spec.description
           version OpenProject::Release::VERSION
 
-          requires_openproject ">= 4.0.0"
+          requires_openproject '>= 4.0.0'
 
-          OpenProject::Footer.add_content('release_version', Proc.new {I18n.t('op_release_current_version', :version => OpenProject::Release.version) if User.current.admin?})
-          OpenProject::Footer.add_content('release_date', Proc.new {I18n.t('op_release_last_update', :released_at => OpenProject::Release.release_date)})
+          OpenProject::Footer.add_content('release_version', proc { I18n.t('op_release_current_version', version: OpenProject::Release.version) if User.current.admin? })
+          OpenProject::Footer.add_content('release_date', proc { I18n.t('op_release_last_update', released_at: OpenProject::Release.release_date) })
         end
       end
     end
